4e2107c1a9138706ce40268ac5849d797b1cb4b5
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-04-17 19:11:37 +0200

    Expand one-model test for create_models.

--- R/manyregs.R
+++ R/manyregs.R
@@ -5,7 +5,15 @@
 #' @param adjustments List of character vectors with names of adjustment
 #'     variables
 #' @param f Function for fitting a model to a dataset.
+#' @return List of models where every model is an object of S3 class
+#'     "manyregs_model".
 #'
 #' @export
 create_models <- function(outcomes, exposures, adjustments, f) {
+    m <- list(
+        outcome = outcomes[1L],
+        exposure = exposures[1L],
+        adjustment = adjustments[[1L]],
+        f = f, fname = deparse(substitute(f)))
+    list(m)
 }
--- tests/testthat/test-create_models.R
+++ tests/testthat/test-create_models.R
@@ -1,5 +1,17 @@
 context("create_models")
 
-test_that("happy path", {
-    create_models("y", "x", list("z"), foo)
+foo <- function() {
+    # empty
+}
+
+test_that("one model", {
+    models <- create_models("y", "x", list(c("z1", "z2")), foo)
+    expect_equal(1, length(models))
+    m <- models[[1]]
+    expect_true(all(c("outcome", "exposure", "adjustment", "f", "fname") %in% names(m)))
+    expect_equal("y", m$outcome)
+    expect_equal("x", m$exposure)
+    expect_equal(c("z1", "z2"), m$adjustment)
+    expect_equal(foo, m$f)
+    expect_equal("foo", m$fname)
 })
