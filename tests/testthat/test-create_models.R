f94c8b66d449bed4bd0779b4b7b4838fe4b170c2
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-04-17 20:01:04 +0200

    Refactor create_models test cases.

--- tests/testthat/test-create_models.R
+++ tests/testthat/test-create_models.R
@@ -4,52 +4,34 @@ foo <- function() {
     # empty
 }
 
+# Return TRUE if, and only if, one of the models matches in terms of
+# outcome, exposure, and so on.
+has_model_with <- function(models, outcome, exposure, adjustment, f, fname) {
+    is_match <- function(m) {
+        outcome == m$outcome &&
+            exposure == m$exposure &&
+            all(adjustment == m$adjustment) &&
+            identical(f, m$f) &&
+            fname == m$fname
+    }
+    any(vapply(models, is_match, logical(1)))
+}
+
 test_that("1 outcome, 1 exposure, 1 adjustment", {
-    outcome <- "y"
-    exposure <- "x"
-    adjustment <- c("z1", "z2")
-    models <- create_models(outcome, exposure, list(adjustment), foo)
+    models <- create_models("y", "x", list(c("z1", "z2")), foo)
     expect_equal(1, length(models))
-    m <- models[[1]]
-    expect_equal(outcome, m$outcome)
-    expect_equal(exposure, m$exposure)
-    expect_equal(adjustment, m$adjustment)
-    expect_equal(foo, m$f)
-    expect_equal("foo", m$fname)
+    expect_true(has_model_with(models, "y", "x", c("z1", "z2"), foo, "foo"))
 })
 
 test_that("2 outcomes, 1 exposure, 1 adjustment", {
-    outcomes <- c("y1", "y2")
-    exposure <- "x"
-    adjustment <- c("z1", "z2")
-    models <- create_models(outcomes, exposure, list(adjustment), foo)
+    models <- create_models(c("y1", "y2"), "x", list(c("z1", "z2")), foo)
     expect_equal(2, length(models))
-    # Model 1
-    m1 <- models[[1]]
-    expect_equal(outcomes[1], m1$outcome)
-    expect_equal(exposure, m1$exposure)
-    expect_equal(adjustment, m1$adjustment)
-    expect_equal(foo, m1$f)
-    expect_equal("foo", m1$fname)
-    # Model 2
-    m2 <- models[[2]]
-    expect_equal(outcomes[2], m2$outcome)
-    expect_equal(exposure, m2$exposure)
-    expect_equal(adjustment, m2$adjustment)
-    expect_equal(foo, m2$f)
-    expect_equal("foo", m2$fname)
+    expect_true(has_model_with(models, "y1", "x", c("z1", "z2"), foo, "foo"))
+    expect_true(has_model_with(models, "y2", "x", c("z1", "z2"), foo, "foo"))
 })
 
-
 test_that("1 outcome, 1 exposure, 0 adjustment", {
-    outcome <- "y"
-    exposure <- "x"
-    models <- create_models(outcome, exposure, f = foo)
+    models <- create_models("y", "x", f = foo)
     expect_equal(1, length(models))
-    m <- models[[1]]
-    expect_equal(outcome, m$outcome)
-    expect_equal(exposure, m$exposure)
-    expect_equal(NULL, m$adjustment)
-    expect_equal(foo, m$f)
-    expect_equal("foo", m$fname)
+    expect_true(has_model_with(models, "y", "x", NULL, foo, "foo"))
 })
