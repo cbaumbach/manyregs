236f38f2df82ba79d8375d10d304750ace217c23
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-04-17 17:55:03 +0200

    Generate initial package documentation.

---
+++ R/pkgname.R
@@ -0,0 +1,6 @@
+#' manyregs --- Flexible and concise regression analysis
+#'
+#' The manyregs package provides a concise and yet flexible way to
+#' specify, fit, and summarize potentially large numbers of regression
+#' models.
+"_PACKAGE"
