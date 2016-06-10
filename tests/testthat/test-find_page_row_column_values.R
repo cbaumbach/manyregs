7ebbff09b740e73271df00e6800252631a00ac94
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-06-10 14:46:19 +0200

    Bundle arguments to find_page_row_column_values.

--- tests/testthat/test-find_page_row_column_values.R
+++ tests/testthat/test-find_page_row_column_values.R
@@ -2,9 +2,8 @@ context("find_page_row_column_values")
 
 test_that("happy path", {
     variables <- list(outcomes = "y", exposures = "x", adjustments = list("z"))
-    rows <- "outcomes"
-    columns <- "exposures"
-    actual <- find_page_row_column_values(variables, rows, columns)
-    expected <- list(page = list("z"), row = "y", column = "x")
+    types <- c(pages = "adjustments", rows = "outcomes", columns = "exposures")
+    actual <- find_page_row_column_values(variables, types)
+    expected <- list(pages = list("z"), rows = "y", columns = "x")
     expect_equal(actual, expected)
 })
