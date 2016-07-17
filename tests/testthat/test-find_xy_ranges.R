6e901efae42bc475902c188d6c4cb029ce6e65e3
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-07-17 12:31:51 +0200

    Refactor test case.

--- tests/testthat/test-find_xy_ranges.R
+++ tests/testthat/test-find_xy_ranges.R
@@ -2,9 +2,10 @@ context("find_xy_ranges")
 
 source("setup.R")
 
-test_that("find_xy_ranges", {
+test_that("multiple models", {
     xylim <- find_xy_ranges(fitted_models)
     ci <- do.call(rbind, lapply(fitted_models, find_exposure_confidence_intervals))
     expect_equal(xylim$xlim, c(1 - .5, 3 + .5))
-    expect_equal(xylim$ylim, c(min(ci$lcl), max(ci$ucl)))
+    expect_equal(xylim$ylim, range(c(ci$lcl, ci$ucl)))
+})
 })
