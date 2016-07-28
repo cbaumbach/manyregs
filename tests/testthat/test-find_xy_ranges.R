5208b46aa56c604c031b69f5635ed38fad007987
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-07-28 23:24:31 +0200

    Refactor find_xy_ranges test cases.

--- tests/testthat/test-find_xy_ranges.R
+++ tests/testthat/test-find_xy_ranges.R
@@ -3,16 +3,19 @@ context("find_xy_ranges")
 source("setup.R")
 
 test_that("multiple models", {
-    xylim <- find_xy_ranges(fitted_models)
-    ci <- do.call(rbind, lapply(fitted_models, find_exposure_estimates))
-    expect_equal(xylim$xlim, c(1 - .5, 3 + .5))
-    expect_equal(xylim$ylim, range(c(ci$lcl, ci$ucl)))
+    models <- fitted_models[1:2]
+    actual <- find_xy_ranges(models)
+    x <- summarize_models(models)
+    ci <- x[x$variable == "x11", c("lcl", "ucl")]
+    expected <- list(xlim = c(1 - .5, 2 + .5), ylim = range(c(ci$lcl, ci$ucl)))
+    expect_equal(actual, expected)
 })
 
 test_that("one model", {
-    model <- fitted_models[[1]]
-    xylim <- find_xy_ranges(list(model))
-    ci <- find_exposure_estimates(model)
-    expect_equal(xylim$xlim, c(1 - .5, 2 + .5))
-    expect_equal(xylim$ylim, range(c(ci$lcl, ci$ucl)))
+    model <- fitted_models[1]
+    actual <- find_xy_ranges(model)
+    x <- summarize_models(model)
+    ci <- x[x$variable == "x11", c("lcl", "ucl")]
+    expected <- list(xlim = c(1 - .5, 2 + .5), ylim = range(c(ci$lcl, ci$ucl)))
+    expect_equal(actual, expected)
 })
