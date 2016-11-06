36e6711a63cb202e18689a7887ed70d20a99cea1
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-11-06 23:00:59 +0100

    Refactor test cases.

--- tests/testthat/test-fit_model.R
+++ tests/testthat/test-fit_model.R
@@ -1,24 +1,38 @@
 context("fit_model")
 
 test_that("happy path", {
-    f <- function(model, data) as.character(model)
+    f <- function(model, data) "fit"
     model <- new_model("y", "x", "z", f, "f")
     fitted_model <- fit_model(model, NULL)
-    expect_equal(fitted_model$fit, as.character(model))
+    expect_equal(fitted_model$fit, "fit")
 })
 
-test_that("warnings are caught, remembered by the model, and displayed", {
+test_that("warnings are shown", {
+    model <- new_model("y", "x", "z", warning, "f")
+    expect_warning(fit_model(model, NULL))
+})
+
+test_that("warnings are saved in the model object", {
     f <- function(model, data) warning("a warning")
     model <- new_model("y", "x", "z", f, "f")
-    expect_warning(fitted_model <- fit_model(model, NULL))
+    suppressWarnings(fitted_model <- fit_model(model, NULL))
     expect_equal(fitted_model$warning, "a warning")
-    expect_null(fitted_model$fit)
 })
 
-test_that("errors are caught, remembered by the model, and displayed as warnings", {
+test_that("errors are converted to warnings", {
+    model <- new_model("y", "x", "z", stop, "f")
+    expect_warning(fit_model(model, NULL))
+})
+
+test_that("errors are saved in the model object", {
     f <- function(model, data) stop("an error")
     model <- new_model("y", "x", "z", f, "f")
-    expect_warning(fitted_model <- fit_model(model, NULL))
+    suppressWarnings(fitted_model <- fit_model(model, NULL))
     expect_equal(fitted_model$error, "an error")
+})
+
+test_that("the fit is NULL if an error occurs", {
+    model <- new_model("y", "x", "z", stop, "f")
+    suppressWarnings(fitted_model <- fit_model(model, NULL))
     expect_null(fitted_model$fit)
 })
