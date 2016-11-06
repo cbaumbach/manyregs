ac95522ee23c09f59b2ea4f6230a3bd56349a321
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-11-06 23:46:38 +0100

    Refactor test cases.

--- tests/testthat/test-find_position_in_layout.R
+++ tests/testthat/test-find_position_in_layout.R
@@ -1,33 +1,40 @@
-context("find_position_in_layout")
+source("setup.R")
 
-sorted_models <- sort_models_for_plotting(models, rows = "exposures", columns = "outcomes")
-layout_info <- find_layout(find_number_of("exposures", sorted_models), find_number_of("outcomes", models))
-
-expect_positions_helper <- function(position, true_positions) {
+expect_positions <- function(model_numbers, true_positions, layout_info) {
     all_positions <- c("bottom", "left", "top", "right")
     false_positions <- setdiff(all_positions, true_positions)
-    for (pos in true_positions)
-        expect_true(position[[pos]])
-    for (pos in false_positions)
-        expect_false(position[[pos]])
-}
-
-expect_positions <- function(model_numbers, true_positions) {
     for (model_number in model_numbers) {
         position <- find_position_in_layout(model_number, layout_info)
-        expect_positions_helper(position, true_positions)
+        for (pos in true_positions) {
+            if (!position[[pos]]) {
+                fail(paste0("Model ", model_number, " should be in position \"", pos, "\""))
+                return()
+            }
+        }
+        for (pos in false_positions) {
+            if (position[[pos]]) {
+                fail(paste0("Model ", model_number, " shouldn't be in position \"", pos, "\""))
+                return()
+            }
+        }
     }
+    succeed()
 }
 
-# page 1:  1 2   page 2:   7  8   page 3:  13 14
-#          3 4             9 10            15 16
-#          5 6            11 12            17 18
+context("find_position_in_layout")
 
 test_that("find_position_in_layout", {
-    expect_positions(c(1, 7, 13), c("left", "top"))
-    expect_positions(c(2, 8, 14), c("right", "top"))
-    expect_positions(c(3, 9, 15), c("left"))
-    expect_positions(c(4, 10, 16), c("right"))
-    expect_positions(c(5, 11, 17), c("left", "bottom"))
-    expect_positions(c(6, 12, 18), c("right", "bottom"))
+    sorted_models <- sort_models_for_plotting(models, rows = "exposures", columns = "outcomes")
+    layout_info <- find_layout(find_number_of("exposures", sorted_models), find_number_of("outcomes", models))
+
+    # page 1:  1 2   page 2:   7  8   page 3:  13 14
+    #          3 4             9 10            15 16
+    #          5 6            11 12            17 18
+
+    expect_positions(c(1, 7, 13), c("left", "top"), layout_info)
+    expect_positions(c(2, 8, 14), c("right", "top"), layout_info)
+    expect_positions(c(3, 9, 15), c("left"), layout_info)
+    expect_positions(c(4, 10, 16), c("right"), layout_info)
+    expect_positions(c(5, 11, 17), c("left", "bottom"), layout_info)
+    expect_positions(c(6, 12, 18), c("right", "bottom"), layout_info)
 })
